8577d721342f19548fbb3cc6bca159fd3cb345ff
VitaliyVorobyov <123983999+VitaliyVorobyov@users.noreply.github.com>
2024-02-13 02:02:22 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from pprint import pprint
-# читаем адресную книгу в формате CSV в список contacts_list
 import csv
+import re
+
 
 with open("phonebook_raw.csv", encoding="utf-8") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
 new_list = []
 for i in contacts_list:
-    name = " ".join(i[: 3]).split()
-    name.append('') if len(name) < 3 else name
-    new_list.append(name+i[3:])
-print(new_list)
-# TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
-
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
+    name = " ".join(i[: 3]).split(' ')[:3]
+    search_phone = re.findall(r'\d+', i[5])
+    if len(search_phone) > 0:
+        full_phone = ''.join(search_phone)
+        phone = re.sub(
+            r'\+?(\d{1})\s?\(?(\d{3})\)?[-\s]?(\d{3})[-\s]?(\d{2})?[-\s]?(\d{2})?',
+            r'+7(\2)\3-\4-\5', full_phone[:11])
+        add_phone = re.sub(r'\s?\(?(\w{3}\.?)?\s?(\d+)\)?', r'доб.\1\2', full_phone[11:])
+        i[5] = ' '.join([phone, add_phone])
+        new_list.append(name + i[3:])
 with open("phonebook.csv", "w", encoding="utf-8") as f:
     datawriter = csv.writer(f, delimiter=',')
-    # Вместо contacts_list подставьте свой список
-    datawriter.writerows(contacts_list)
+    datawriter.writerows(new_list)
