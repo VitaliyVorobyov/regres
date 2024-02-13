aaed4b9c5602b3d9f87acbc5f3993c401124b70f
VitaliyVorobyov <123983999+VitaliyVorobyov@users.noreply.github.com>
2024-02-13 13:45:10 +0300

    bag fix

--- main.py
+++ main.py
@@ -6,17 +6,19 @@ with open("phonebook_raw.csv", encoding="utf-8") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
 new_list = []
-for i in contacts_list:
-    name = " ".join(i[: 3]).split(' ')[:3]
-    search_phone = re.findall(r'\d+', i[5])
-    if len(search_phone) > 0:
-        full_phone = ''.join(search_phone)
-        phone = re.sub(
-            r'\+?(\d{1})\s?\(?(\d{3})\)?[-\s]?(\d{3})[-\s]?(\d{2})?[-\s]?(\d{2})?',
-            r'+7(\2)\3-\4-\5', full_phone[:11])
-        add_phone = re.sub(r'\s?\(?(\w{3}\.?)?\s?(\d+)\)?', r'доб.\1\2', full_phone[11:])
-        i[5] = ' '.join([phone, add_phone])
-        new_list.append(name + i[3:])
+for person in contacts_list:
+    name = " ".join(person[: 3]).split(' ')[:3]
+    search_phone = re.findall(r'\d+', person[5])
+    full_phone = ''.join(search_phone)
+    phone = re.sub(r'\+?(\d)(\d{3})(\d{3})(\d{2})(\d{2})', r'+7(\2)\3-\4-\5', full_phone[:11])
+    add_phone = re.sub(r'(\d+)', r'доб.\1', full_phone[11:])
+    if add_phone == '':
+        person[5] = phone
+    else:
+        person[5] = ' '.join([phone, add_phone])
+    new_list.append(name + person[3:])
+
+
 with open("phonebook.csv", "w", encoding="utf-8") as f:
     datawriter = csv.writer(f, delimiter=',')
     datawriter.writerows(new_list)
